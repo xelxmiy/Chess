6f3b6bc857bbcd1730ae19343f558c37e89f8446
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2023-06-18 23:22:37 -0500

    Update King.java

--- src/chessnt/chesspieces/King.java
+++ src/chessnt/chesspieces/King.java
@@ -12,13 +12,18 @@ import chessnt.StyleClasses.StyleManager;
  * @version 1.0.0
  */
 public class King extends Piece {
-    
+
     public static boolean isInCheck = false;
-    
+
+    private static int[][] validMoves = {
+        {-1, -1}, {-1, 0}, {-1, +1},
+        {0, -1}, {0, +1},
+        {+1, -1}, {+1, 0}, {+1, +1}};
+
     public King(int row, int column, boolean isWhite) {
-        
+
         super(row, column, isWhite);
-                    
+
         this.row = row;
         this.column = column;
 
@@ -30,7 +35,25 @@ public class King extends Piece {
 
     @Override
     public boolean isValidMove(int moveRow, int moveColumn) {
-        return true;
+
+        //check to prevent the king capturing one of it's own pieces
+        if (Chess.board.get(moveRow, moveColumn) != null) {
+            if (Chess.board.get(moveRow, moveColumn).isLight == isLight) {
+                return false;
+            }
+        }
+
+        //checks if the piece is moving to the same square, skipping a turn
+        if (moveRow == row && moveColumn == column) {
+            return false;
+        }
+        //checks if the location is one of the valid king moves
+        for (int[] validMove : validMoves) {
+            if (moveRow == (row + validMove[0]) && (moveColumn == (column + validMove[1]))) {
+                return true;
+            }
+        }
+        return false ;
     }
-    
+
 }
